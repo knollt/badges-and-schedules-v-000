77cc2e071c0b3b5c477d263c83b2740c2565ab15
Tim Knoll <timothy.knoll@gmail.com>
2019-11-29 17:52:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,16 @@
 # Write your code here.
-def badge_maker (name)
+
+def badge_maker (name) #print badges of speaker
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator (speakers)
+def batch_badge_creator (speakers) #takes list of names and return badge_message
   badge_message = []
   speakers.each {|speaker| badge_message.push(badge_maker(speaker))}
   return badge_message
 end
 
-def assign_rooms (speakers)
+def assign_rooms (speakers) #takes list of speakers & assigns each speaker to a room
   room_number = 1
   room_message = []
   speakers.each do |speaker|
@@ -19,7 +20,7 @@ def assign_rooms (speakers)
   return room_message
 end
 
-def printer (speakers)
+def printer (speakers) # output the results ffrom batch_badge_creator
   badge_message = batch_badge_creator(speakers)
   badge_message.each {|message| puts message}
 
@@ -28,28 +29,50 @@ def printer (speakers)
 
 end
 
-# def badge_maker(name)
-#   "Hello, my name is #{name}."
-# end
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+<<<<<<< HEAD
 #
+=======
+# 
+>>>>>>> d8bf6402fba545a73df69391c2190361f978c616
 # def batch_badge_creator(attendees)
 #   attendees.map do |attendee|
 #     "Hello, my name is #{attendee}."
 #   end
 # end
+<<<<<<< HEAD
 #
+=======
+# 
+>>>>>>> d8bf6402fba545a73df69391c2190361f978c616
 # def assign_rooms(attendees)
 #   attendees.each_with_index.map do |attendee, index|
 #     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 #   end
 # end
+<<<<<<< HEAD
 #
+=======
+# 
+>>>>>>> d8bf6402fba545a73df69391c2190361f978c616
 # def printer(attendees)
 #   batch_badge_creator(attendees).each do |badge|
 #     puts badge
 #   end
+<<<<<<< HEAD
 #
 #   assign_rooms(attendees).each do |assignment|
 #     puts assignment
 #   end
 # end
+=======
+# 
+#   assign_rooms(attendees).each do |assignment|
+#     puts assignment
+#   end
+# end
+>>>>>>> d8bf6402fba545a73df69391c2190361f978c616
